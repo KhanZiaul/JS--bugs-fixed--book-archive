c6086b0734025bb1627bd53cfed73c90ed71f07c
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-03-13 01:40:15 +0600

    search field validation done

--- js/script.js
+++ js/script.js
@@ -1,9 +1,21 @@
 //------------- handle search button-----------
+const error = document.getElementById('error');
+
+const searchResult = document.getElementById("search-result");
+
 document.getElementById('search').addEventListener('click',function(){
 
   const searchField = document.getElementById("search-input");
   const searchText = searchField.value;
-  searchBook(searchText);
+  if(searchText.length == 0){
+    error.innerText = "search field can not be empty"
+    alert('Hello,are you mad!! enter something')
+    searchResult.innerHTML = '';
+  }
+  else{
+    error.innerText = '' ;
+    searchBook(searchText);
+  }
 })
 
 const searchBook = (searchText) => {
@@ -19,7 +31,6 @@ const searchBook = (searchText) => {
 
 // ----------display search result data----------
 const displaySearchResult = (myBooks) => {
-  const searchResult = document.getElementById("search-result");
   searchResult.innerText = "";
   const totalBooks = document.getElementById('total');
   totalBooks.innerHTML = `<h3 class="text-center text-secondary">Total books found ${myBooks.docs.length}</h3>`;
@@ -29,12 +40,12 @@ const displaySearchResult = (myBooks) => {
   div.classList.add("col");
   div.innerHTML = `
       <div class="card">
-      <img src=" https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top w-50 mx-auto" alt="...">
+      <img src=" https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top w-50 mx-auto" alt="no image">
         <div class="card-body text-center">
-            <h5 class="card-title"> Name: ${book.title?book.title : 'No Name Found'}</h5>
-            <h6> Author: ${book.author_name}</h6>
-            <p>Publisher: <small> ${book.publisher?book.publisher[0]:'No publisher found'} </small></p>
-            <small> First Published Year: ${book.first_publish_year?book.first_publish_year: 'No Year Found'}</small>
+            <h5 class="card-title"> Name: ${book.title?book.title : 'Unknown'}</h5>
+            <h6> Author: ${book.author_name?book.author_name : 'Unknown'}</h6>
+            <p>Publisher: <small> ${book.publisher?book.publisher[0]:'Unknown'} </small></p>
+            <small> First Published Year: ${book.first_publish_year?book.first_publish_year: 'Unknown'}</small>
           </div>
       </div>
       `;
@@ -42,12 +53,3 @@ const displaySearchResult = (myBooks) => {
   
   });
 };
-
-
-
-{/* */}
-           
-
-
-
-searchBook();
