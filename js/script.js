80aa4ebeb1041cb20a6f1b1b7e6b56f0e093358c
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-03-13 01:19:05 +0600

    bugs: book cover image fixed

--- js/script.js
+++ js/script.js
@@ -11,20 +11,26 @@ const searchBook = (searchText) => {
   const url = `https://openlibrary.org/search.json?q=${searchText}`;
   fetch(url)
     .then((res) => res.json())
-    .then((data) => displaySearchResult(data));
+    .then((data) => {
+      console.log(data);
+      displaySearchResult(data)
+    });
 };
 
 // ----------display search result data----------
 const displaySearchResult = (myBooks) => {
   const searchResult = document.getElementById("search-result");
   searchResult.innerText = "";
-  myBooks.docs.forEach((book) => {
-  console.log(book);
+  const totalBooks = document.getElementById('total');
+  totalBooks.innerHTML = `<h3 class="text-center text-secondary">Total books found ${myBooks.docs.length}</h3>`;
+  myBooks.docs.forEach((book) =>{
+  let code = book.cover_i;
   const div = document.createElement("div");
   div.classList.add("col");
   div.innerHTML = `
       <div class="card">
-         
+      <img src=" https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top w-50 mx-auto" alt="...">
+        <div class="card-body text-center">
             <h5 class="card-title"> Name: ${book.title?book.title : 'No Name Found'}</h5>
             <h6> Author: ${book.author_name}</h6>
             <p>Publisher: <small> ${book.publisher?book.publisher[0]:'No publisher found'} </small></p>
@@ -39,9 +45,9 @@ const displaySearchResult = (myBooks) => {
 
 
 
-// <img src=" https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top w-50 mx-auto" alt="...">
-//             <div class="card-body text-center"></div>
+{/* */}
+           
 
 
 
-// searchBook();
+searchBook();
